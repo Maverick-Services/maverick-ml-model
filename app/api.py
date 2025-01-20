234e3806933ece0e5dfb631a8d31141ec9598d1e
Maverick-Services <maverickit123@gmail.com>
2025-01-20 14:24:42 +0530

    app.utils--correct

--- app/api.py
+++ app/api.py
@@ -3,7 +3,7 @@ import logging
 import os
 import string
 import nltk
-from utils import predict_intent, generate_response
+from app.utils import predict_intent, generate_response
 from memory_profiler import profile 
 import gc 
 from flask_caching import Cache
